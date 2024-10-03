c4c48716eede97c690b760b3f9db732bc787747d
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-10-02 19:58:49 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+def list_files(startpath : str):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+def main():
+    list_files(os.getcwd())
+if "__main__" == __name__:
+    main()
